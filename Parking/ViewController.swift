a3e6d4c0dba629fe9a59c8ca5bc9fa63c1d73de6
Caelan Dailey <caelandailey@gmail.com>
2018-08-28 21:16:50 -0600

    Initial Commit

---
+++ Parking/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Parking
+//
+//  Created by Caelan Dailey on 8/28/18.
+//  Copyright © 2018 Caelan Dailey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
